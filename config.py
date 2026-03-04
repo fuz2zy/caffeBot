969ae00f89e8ba363eb0daf1d59ca281f45221de
fuz2zy <fuzzy.garp@gmail.com>
2026-03-04 22:32:48 +0300

    start of bot development

---
+++ config.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import logging
+import os
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", 0)
+ADMIN_ID = os.getenv("ADMIN_ID", 0)
+
+
+if not BOT_TOKEN:
+    logging.critical("Токен бота не найден, бот не может быть запущен")
+if not ADMIN_ID:
+    logging.warning("Айди администратора бота не найден")
